180304bc1c6cd413e016d360cca1dfa2d24b6c98
aryagiat <arya.giat@gmail.com>
2021-02-07 09:58:49 +0800

    copy paste the template from main

--- CoDrone Examples/FlightController/helper.cpp
+++ CoDrone Examples/FlightController/helper.cpp
@@ -3,30 +3,49 @@
 #include "helper.h"
 #include "setting.h"
 
+/**
+ * Read the yaw value from the joystick.
+ * @return Manipulated yaw analog value from the joystick.
+ */
 int readYawValue() {
-   int yawValue = -1 * CoDrone.AnalogScaleChange(analogRead(A3));
+   int yawValue = CoDrone.AnalogScaleChange(analogRead(A3));
    // recalibrate the range 
-   return yawValue / SCALE - yawOffset;
+   return -1 * ((yawValue - yawOffset) / SCALE);
 }
 
+/**
+ * Read the roll value from the joystick.
+ * @return Manipulated roll analog value from the joystick.
+ */
 int readRollValue() {
-   int rollValue = -1 * CoDrone.AnalogScaleChange(analogRead(A5));
+   int rollValue = CoDrone.AnalogScaleChange(analogRead(A4));
    // recalibrate the range 
-   return rollValue / SCALE - rollOffset;
+   return -1 * ((rollValue - rollOffset) / SCALE);
 }
 
+/**
+ * Read the throttle value from the joystick.
+ * @return Manipulated throttle analog value from the joystick.
+ */
 int readThrottleValue() {
-   int throttleValue = CoDrone.AnalogScaleChange(analogRead(A4));
+   int throttleValue = CoDrone.AnalogScaleChange(analogRead(A5));
    // recalibrate the range 
-   return throttleValue / SCALE - throttleOffset;
+   return (throttleValue - throttleOffset) / SCALE;
 }
 
+/**
+ * Read the pitch value from the joystick.
+ * @return Manipulated pitch analog value from the joystick.
+ */
 int readPitchValue() {
    int pitchValue = CoDrone.AnalogScaleChange(analogRead(A6));
    // recalibrate the range 
-   return pitchValue / SCALE - pitchOffset;
+   return (pitchValue - pitchOffset) / SCALE;
 }
 
+/**
+ * Calibrate the joystick by setting the offsets.
+ */
 void recalibrate(){
   rollOffset = CoDrone.AnalogScaleChange(analogRead(rollPort));
   pitchOffset = CoDrone.AnalogScaleChange(analogRead(pitchPort));
@@ -34,18 +53,30 @@ void recalibrate(){
   throttleOffset = CoDrone.AnalogScaleChange(analogRead(throttlePort));
 }
 
+/**
+ * Getting the left IR sensor.
+ */
 int getLeftSensor(){
    return analogRead(19);
 }
 
+/**
+ * Getting the middle IR sensor.
+ */
 int getMiddleSensor(){
    return analogRead(20);
 }
 
+/**
+ * Getting the right IR sensor.
+ */
 int getRightSensor(){
    return analogRead(21);
 }
 
+/**
+ * Displaying the offsets.
+ */
 void displaySetting(){
    Serial.print("yawOffset : "); 
    Serial.println(yawOffset);
@@ -66,6 +97,9 @@ void displaySetting(){
    Serial.println(RANGEMAX);
 }
 
+/**
+ * Displaying the movements.
+ */
 void displayMovement(){
    Serial.print("Yaw: ");
    Serial.println(readYawValue());
